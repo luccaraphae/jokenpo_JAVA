009c35f3ef18ccdd200f7a6127f1187725be1d2d
Lucca Raphael <luccaraphae@gmail.com>
2021-02-19 18:58:06 -0300

    c04- Programando lógica do jogo

--- app/src/main/java/com/example/jokenpo/tela2.java
+++ app/src/main/java/com/example/jokenpo/tela2.java
@@ -3,12 +3,13 @@ package com.example.jokenpo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import java.util.Random;
 
@@ -25,90 +26,90 @@ public class tela2 extends AppCompatActivity {
         setContentView(R.layout.activity_tela2);
 
         //referenciando aos objetos
-        imgMaquina=(ImageView)findViewById(R.id.imgPC);
-        imgJogador=(ImageView)findViewById(R.id.imgJogador);
-        btPedra=(ImageButton)findViewById(R.id.btn1);
-        btPapel=(ImageButton)findViewById(R.id.btn2);
-        btTesoura=(ImageButton)findViewById(R.id.btn3);
-        txtResultado=(TextView) findViewById(R.id.txtStatus);
+        imgMaquina = findViewById(R.id.imgPC);
+        imgJogador = findViewById(R.id.imgJogador);
+        btPedra = findViewById(R.id.btn1);
+        btPapel = findViewById(R.id.btn2);
+        btTesoura = findViewById(R.id.btn3);
+        txtResultado = findViewById(R.id.txtStatus);
 
 
         btPedra.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
 
                 //chamando a classe Random para sorteio aleatorio
-                 Random randomico= new Random();
+                Random randomico = new Random();
 
                 //bound 3 = 0-pedra, 1-papel,2-tesoura
-                 int numsorteado=randomico.nextInt(3);
+                int numsorteado = randomico.nextInt(3);
 
-                 //Fazendo a seleção para setar a imagem de acordo com a imagem
-                 switch(numsorteado){
+                //Fazendo a seleção para setar a imagem de acordo com indice
+                switch (numsorteado) {
+                    //pedra
+                    case 0: {
+                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.pedra));
+                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.pedra));
+                        txtResultado.setText("EMPATE!");
+                        break;
+                    }
 
-                     //pedra
-                     case 0:{
-                         imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.pedra));
-                         imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.pedra));
-                         txtResultado.setText("EMPATE!");
-                         break;
-                     }
+                    //papel
+                    case 1: {
+                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.papel));
+                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.dpedra));
+                        txtResultado.setText("VOCÊ PERDEU!");
+                        break;
+                    }
 
-                     //papel
-                     case 1:{
-                         imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.papel));
-                         imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.dpedra));
-                         txtResultado.setText("VOCÊ PERDEU!");
-                         break;
-                     }
+                    //tesoura
+                    case 2: {
+                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.dtesoura));
+                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.pedra));
+                        txtResultado.setText("VOCÊ GANHOU!");
+                        break;
+                    }
 
-                     //tesoura
-                     case 2:{
-                         imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.dtesoura));
-                         imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.pedra));
-                         txtResultado.setText("VOCÊ GANHOU!");
-                         break;
-                     }
-
-                 }
+                }
             }
         });
 
 
-
         btPapel.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
 
                 //chamando a classe Random para sorteio aleatorio
-                Random randomico= new Random();
+                Random randomico = new Random();
 
                 //bound 3 = 0-pedra, 1-papel,2-tesoura
-                int numsorteado=randomico.nextInt(3);
+                int numsorteado = randomico.nextInt(3);
 
-                //Fazendo a seleção para setar a imagem de acordo com a imagem
-                switch(numsorteado){
+                //Fazendo a seleção para setar a imagem de acordo com indice
+                switch (numsorteado) {
 
                     //pedra
-                    case 0:{
-                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.dpedra));
-                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.papel));
+                    case 0: {
+                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.dpedra));
+                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.papel));
                         txtResultado.setText("VOCÊ GANHOU!");
                         break;
                     }
 
                     //papel
-                    case 1:{
-                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.papel));
-                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.papel));
+                    case 1: {
+                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.papel));
+                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.papel));
                         txtResultado.setText("EMPATE!");
                         break;
                     }
 
                     //tesoura
-                    case 2:{
-                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.tesoura));
-                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.dpapel));
+                    case 2: {
+                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.tesoura));
+                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.dpapel));
                         txtResultado.setText("VOCÊ PERDEU!");
                         break;
                     }
@@ -118,40 +119,40 @@ public class tela2 extends AppCompatActivity {
         });
 
 
-
         btTesoura.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
             public void onClick(View v) {
 
                 //chamando a classe Random para sorteio aleatorio
-                Random randomico= new Random();
+                Random randomico = new Random();
 
                 //bound 3 = 0-pedra, 1-papel,2-tesoura
-                int numsorteado=randomico.nextInt(3);
+                int numsorteado = randomico.nextInt(3);
 
-                //Fazendo a seleção para setar a imagem de acordo com a imagem
-                switch(numsorteado){
+                //Fazendo a seleção para setar a imagem de acordo com indice
+                switch (numsorteado) {
 
                     //pedra
-                    case 0:{
-                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.pedra));
-                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.dtesoura));
+                    case 0: {
+                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.pedra));
+                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.dtesoura));
                         txtResultado.setText("VOCÊ PERDEU!");
                         break;
                     }
 
                     //papel
-                    case 1:{
-                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.dpapel));
-                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.tesoura));
+                    case 1: {
+                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.dpapel));
+                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.tesoura));
                         txtResultado.setText("VOCÊ GANHOU!");
                         break;
                     }
 
                     //tesoura
-                    case 2:{
-                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.tesoura));
-                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this,R.drawable.tesoura));
+                    case 2: {
+                        imgMaquina.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.tesoura));
+                        imgJogador.setImageDrawable(ContextCompat.getDrawable(tela2.this, R.drawable.tesoura));
                         txtResultado.setText("EMPATE!");
                         break;
                     }
